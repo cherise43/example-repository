dc01b61df82c0267fd496d444e2376724dac6ec8
Cherise <nashpaiessy@gmail.com>
2023-06-10 21:06:26 +0300

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,4 @@
+const h2= document.createElement["h2"];
+h2.text.Content = "This content added by Javascript";
+document.querySelector["body"].appendChild[h2];
+<script> src="script.js"{">"}</script>
